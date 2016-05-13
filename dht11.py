1ee72fbf0eda56ee8b8e0c15cc73d1862bd14a88
Justin Duplessis <drfoliberg@gmail.com>
2016-05-13 12:25:46 -0400

    better while

--- dht11.py
+++ dht11.py
@@ -105,7 +105,7 @@ class DHT11:
 
         last = -1
         data = []
-        while True:
+        while unchanged_count > max_unchanged_count:
             current = GPIO.input(self.__pin)
             data.append(current)
 
@@ -114,8 +114,7 @@ class DHT11:
                 last = current
             else:
                 unchanged_count += 1
-                if unchanged_count > max_unchanged_count:
-                    break
+
         print(len(data))
         return data
 
